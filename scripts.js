3474682f677c444adb56579c03d39e096d0bb562
Kvitandze <55856783+Kvitandze@users.noreply.github.com>
2020-01-29 19:01:57 +0400

    sdas

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+
 import axios from 'axios';
 
 // 1)
@@ -18,7 +19,7 @@ setTimeout(() => {
 
 console.log(reject);
 
-},2000);
+},20000);
 
 });
 
@@ -43,3 +44,33 @@ ul.appendChild(li);
 body.appendChild(ul)
 
 })
+
+// 3)
+
+let regex = /^([a-zA-Z]{5})([1258]{5})([AB]{5})$/;
+
+let validation = document.querySelector('.validation').textContent;
+
+var found = validation.match(regex);
+
+if (validation.match(regex)) {
+
+console.log(`Success - ${found}`)
+
+} else {
+
+console.log('undef')
+
+}
+
+// 4)
+
+let p = document.querySelector('.firstP').textContent;
+
+let secondRegex = /([0-9]{4})([0-9]{5})/g;
+
+let pp = document.querySelector('.secP');
+
+let newText = p.replace(secondRegex,'####\$2');
+
+pp.innerHTML = newText;
